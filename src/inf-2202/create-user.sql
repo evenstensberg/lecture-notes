4f96d74ae63ce591a0e21a9507d67a4ace1695b6
evenstensberg <evenstensberg@gmail.com>
2019-08-24 19:52:01 +0200

    chore: 😇

--- src/inf-2202/create-user.sql
+++ src/inf-2202/create-user.sql
@@ -14,3 +14,19 @@ CREATE TABLE users(
     age INTEGER,
     interests VARCHAR(20)
 );
+
+/* Rename */
+ALTER TABLE users RENAME TO notUsers;
+
+
+/* Add column, sql doesn't have bools, so 0 = nay, 1 = yay */
+ALTER TABLE notUsers ADD COLUMN isLit INTEGER;
+
+/* Insert into table */
+INSERT INTO notUsers (uniq_id, first_name, last_name, age, interests, isLit) VALUES (69, "Jeff", "HMM", 21, "Eat, Sleep, Code", 1);
+
+/* Without params */
+INSERT INTO notUsers VALUES(96, "Pingu", "The fish", 12, "Fishing, Swimming", 0);
+
+/* Defaults 🙂 */
+INSERT INTO notUsers DEFAULT VALUES;
